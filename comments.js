1a4260d6f512ea96ae2c55c18450a6745e4c6bf2
Huan Nguyen <32515417+nguyenhuands99@users.noreply.github.com>
2023-11-19 17:20:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,36 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Create app
+const app = express();
+
+// Middleware
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Route
+app.get('/posts/:id/comments', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            content: 'Comment 1'
+        },
+        {
+            id: 2,
+            content: 'Comment 2'
+        },
+        {
+            id: 3,
+            content: 'Comment 3'
+        }
+    ]);
+});
+
+// Start app
+app.listen(4001, () => {
+    console.log('Listening on port 4001');
+});
